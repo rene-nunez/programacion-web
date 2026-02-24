921e32bdb82ade01cbf6d52b064aa6f6d0c8b008
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-02-23 23:12:51 -0600

    feat(actividades): update control-inventario/*
    - Se cambio el botón submit por buttonAdd con validación en JavaScript para el formulario.
    - Se ajusto el CSS del contenedor de notificaciones
    - index.js renombrado a app.js

--- actividades/control-inventario/app.js
+++ actividades/control-inventario/app.js
@@ -25,33 +25,42 @@ class Inventario {
 
 let inventario = new Inventario();
 
-const form = document.getElementById("formProducto");
+const buttonAdd = document.getElementById("buttonAdd");
 
-form.addEventListener("submit", ()=> {
-    event.preventDefault();
-    
+buttonAdd.addEventListener("click", ()=> {
     let codigo = document.getElementById("inputCodigo").value;
     let nombre = document.getElementById("inputNombre").value;
     let cantidad = parseInt(document.getElementById("inputCantidad").value);
     let costo = parseFloat(document.getElementById("inputCosto").value) ;
     let categoria = document.getElementById("inputCategoria").value;
 
-    if(cantidad < 1) {
-        return alert("Cantidad inválida");
-    } 
-    
-    if(costo < 1) {
-        return alert("Costo inválida");
+    // Validación
+    if (isNaN(codigo) || codigo <= 0) {
+        return mostrarNotificacion("Código inválido", "error");
+    }
+    if (!nombre) {
+        return mostrarNotificacion("Nombre inválido", "error");
+    }
+    if (isNaN(cantidad) || cantidad < 1) {
+        return mostrarNotificacion("Cantidad inválida", "error");
+    }
+    if (isNaN(costo) || costo < 1) {
+        return mostrarNotificacion("Costo inválido", "error");
+    }
+    if (!categoria) {
+        return mostrarNotificacion("Categoria inválida", "error");
     }
     
     let producto = new Producto(codigo, nombre, cantidad, costo, categoria);
     
     if(!inventario.agregarProducto(producto)) {
         mostrarNotificacion("Producto ya existe", "error");
+    
     } else {
         inventario.productos.forEach((valor, clave) => {
-            console.log(clave, valor);
+            console.log(clave, valor); // mostrar el inventario en consola
         });
+
         mostrarNotificacion("Producto agregado", "exito");
     }
 });
@@ -59,18 +68,15 @@ form.addEventListener("submit", ()=> {
 function mostrarNotificacion(mensaje, tipo) {
     const contenedor = document.getElementById("notificaciones");
     const notif = document.createElement("div");
+
     notif.classList.add("notificacion", tipo);
     notif.textContent = mensaje;
     contenedor.appendChild(notif);
 
-    // animación de entrada
     setTimeout(() => notif.classList.add("show"), 10);
 
-    // desaparecer después de 3 segundos
     setTimeout(() => {
         notif.classList.remove("show");
-        // remover del DOM después de la animación
         setTimeout(() => contenedor.removeChild(notif), 500);
     }, 3000);
-    Notificacion("Producto agregado", "exito");
 }
