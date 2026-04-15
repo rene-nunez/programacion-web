cc50b6aafb2413e22e0637e612c2e9d47944d87a
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-04-14 23:12:29 -0600

    feat: add app.js file

---
+++ docs/actividades/api-vehiculos/app.js
@@ -0,0 +1,13 @@
+const btnAgregar = document.getElementById("btnAgregar");
+const btnListar = document.getElementById("btnListar");
+const btnBuscar = document.getElementById("btnBuscar");
+
+const lista = document.getElementById("lista");
+
+let placa = document.getElementById("placa").value;   
+let marca = document.getElementById("marca").value;  
+let modelo = document.getElementById("modelo").value;
+
+btnAgregar.addEventListener("click", () => {
+    fetch("")
+})
